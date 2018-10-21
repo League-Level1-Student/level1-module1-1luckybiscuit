ce82862173fcd1671620c1d1ff4aa7cfe1c5c200
1luckybiscuit <league@WTS5.attlocal.net>
2018-10-21 11:31:01 -0700

    To go even further beyond
    I finished the binary converter but we tried printing a lenny face and
    had to create new code using arrays to do it. The lenny face only shows
    up once you put the output into a search bar, but still!!!!!!

--- src/extra/BinaryGendersOnly.java
+++ src/extra/BinaryGendersOnly.java
@@ -1,5 +1,6 @@
 package extra;
 
+import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -11,52 +12,67 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class BinaryGendersOnly implements MouseListener {
+	JFrame de = new JFrame();
+	JPanel spa = new JPanel();
+	JTextField ci = new JTextField(20);
+	JLabel to = new JLabel();
+	JButton two = new JButton("Convert");
 	public static void main(String[] args) {
 		BinaryGendersOnly a = new BinaryGendersOnly();
 		a.setup();
-	}// bbbb
+	}
 
 	void setup() {
-		JFrame de = new JFrame();
-		JPanel spa = new JPanel();
-		JTextField ci = new JTextField(30);
-		JLabel to = new JLabel();
-		JButton two = new JButton();
+		de.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		two.addMouseListener(this);
 		de.add(spa);
 		spa.add(ci);
+		spa.add(to);
 		spa.add(two);
-		two.add(to);
-		two.setText("convert");
+		two.setPreferredSize(new Dimension(80,30));
+		to.setVisible(true);
 		de.setVisible(true);
 		de.setTitle("Converts 8 bits of binary to number");
 		de.pack();
 	}
-
-	String convert(String input) {
-		if (input.length() != 8) {
-			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
-			return "-";
+	void bitSplit(String code) {
+		String[] codes = code.split(" ");
+		String anyLenny = "";
+		for(int i = 0;i<codes.length;i++) {
+			anyLenny += convert(codes[i]);
 		}
-		String binary = "[0-1]+"; // must contain numbers in the given range
-		if (!input.matches(binary)) {
-			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
-			return "-";
-		}
-		try {
-			int asciiValue = Integer.parseInt(input, 2);
-			char theLetter = (char) asciiValue;
-			return "" + theLetter;
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
-			return "-";
-		}
+		to.setText(anyLenny);
+		System.out.println(anyLenny);
 	}
-
+	String convert(String input) {
+        if(input.length() != 8){
+             JOptionPane.showMessageDialog(null, "8 bits per byte, silly!!!");
+             return "-";
+        }
+        String binary = "[0-1]+";    //must contain numbers in the given range
+        if (!input.matches(binary)) {
+             JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+             return "-";
+        }
+        try {
+             int asciiValue = Integer.parseInt(input, 2);
+             char theLetter = (char) asciiValue;
+             return "" + theLetter;
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+             return "-";
+        }
+   }
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, convert(ci));
+		String text = ci.getText();
+//		String label = convert(text);
+//		to.setText(label);
+//		System.out.println(text);
+//		System.out.println(label);
+		bitSplit(text);
+		de.pack();
 	}
 
 	@Override
